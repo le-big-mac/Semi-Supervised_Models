0e20cbf86663939540ae39da65d28ea12892c336
Charlie <cl685@cam.ac.uk>
2019-03-25 18:17:09 +0000

    ladderfromtf

--- LadderFromTF.py
+++ LadderFromTF.py
@@ -220,7 +220,7 @@ for epoch in range(50):
         labels = labels.to(device)
 
         unlabelled_images, _ = unlabelled_data
-        unlabelled_images = unlabelled_data.to(device)
+        unlabelled_images = unlabelled_images.to(device)
 
         inputs = torch.cat((labelled_images, unlabelled_images), 0)
 
