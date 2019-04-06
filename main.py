4ea6bb881f6048859ac5b776a7d3bce6d5211d7a
Charlie <cl685@cam.ac.uk>
2019-04-06 14:00:32 +0100

    changes to main

--- main.py
+++ main.py
@@ -6,51 +6,30 @@ import argparse
 from utils.datautils import load_MNIST_data, save_results, load_data_from_file
 from Models import SimpleNetwork, PretrainingNetwork, SDAE, SimpleM1, M1, M2Runner, LadderNetwork
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-state_path = './Models/state'
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Take arguments to construct model')
-    parser.add_argument('model', type=str,
-                        choices=['simple', 'pretraining', 'sdae', 'simple_m1', 'm1', 'm2', 'ladder'],
-                        help="Choose which model to run"
-                        )
-    args = parser.parse_args()
-
-    model_name = args.model
-    unsupervised_batch_size = 1000
-    batch_size = 100
-
-    dataset_name = 'MNIST'
-    unsupervised_dataset, supervised_dataset, validation_dataset, test_dataset = \
-        load_MNIST_data(100, 2000, True, True)
+def get_models_and_dataloaders(model_name, dataset_name, datasets, batch_size, unsupervised_batch_size):
+    model = None
+    unsupervised_dataset, supervised_dataset, validation_dataset, test_dataset = datasets
 
     unsupervised_dataloader = DataLoader(unsupervised_dataset, batch_size=unsupervised_batch_size, shuffle=True)
     supervised_dataloader = DataLoader(supervised_dataset, batch_size=batch_size, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=validation_dataset.__len__())
     test_dataloader = DataLoader(test_dataset, batch_size=test_dataset.__len__())
 
-    if not os.path.exists(state_path):
-        os.mkdir(state_path)
-    if not os.path.exists('{}/{}'.format(state_path, model_name)):
-        os.mkdir('{}/{}'.format(state_path, model_name))
-
-    model = None
-    dataloaders = (supervised_dataloader, unsupervised_dataloader, validation_dataloader)
+    train_dataloaders = (unsupervised_dataloader, supervised_dataloader, validation_dataloader)
 
     if model_name == 'simple':
         model = SimpleNetwork(784, [1000, 500, 250, 250, 250], 10, dataset_name, device)
-        dataloaders = (supervised_dataloader, validation_dataloader)
+        train_dataloaders = (supervised_dataloader, validation_dataloader)
 
     elif model_name == 'pretraining':
-        model = PretrainingNetwork(784, [1000, 500, 250, 250, 250], 10, lambda x: x, nn.Sigmoid(), dataset_name, device)
+        model = PretrainingNetwork(784, [1000, 500, 250, 250, 250], 10, nn.Sigmoid(), dataset_name, device)
 
     elif model_name == 'sdae':
         model = SDAE(784, [1000, 500, 250, 250, 250], 10, nn.ReLU(), dataset_name, device)
 
     elif model_name == 'simple_m1':
-        model = SimpleM1(784, [256, 128], 32, [32], 10, lambda x: x, nn.Sigmoid(), dataset_name, device)
+        model = SimpleM1(784, [256, 128], 32, [32], 10, nn.Sigmoid(), dataset_name, device)
 
     elif model_name == 'm1':
         model = M1(784, [256, 128], 32, [32], 10, nn.Sigmoid(), dataset_name, device)
@@ -59,23 +38,48 @@ def main():
         model = M2Runner(784, [256, 128], [256], 32, 10, nn.Sigmoid(), dataset_name, device)
 
         unsupervised_dataloader = DataLoader(unsupervised_dataset, batch_size=batch_size, shuffle=True)
-        dataloaders = (supervised_dataloader, unsupervised_dataloader, validation_dataloader)
+        train_dataloaders = (supervised_dataloader, unsupervised_dataloader, validation_dataloader)
 
     elif model_name == 'ladder':
         model = LadderNetwork(784, [1000, 500, 250, 250, 250], 10, [1000.0, 10.0, 0.10, 0.10, 0.10, 0.10, 0.10],
                               dataset_name, device)
 
         unsupervised_dataloader = DataLoader(unsupervised_dataset, batch_size=batch_size, shuffle=True)
-        dataloaders = (supervised_dataloader, unsupervised_dataloader, validation_dataloader)
+        train_dataloaders = (supervised_dataloader, unsupervised_dataloader, validation_dataloader)
 
-    print('here')
+    return model, train_dataloaders, test_dataloader
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Take arguments to construct model')
+    parser.add_argument('model', type=str,
+                        choices=['simple', 'pretraining', 'sdae', 'simple_m1', 'm1', 'm2', 'ladder'],
+                        help="Choose which model to run"
+                        )
+    parser.add_argument('--max_epochs', type=int, default=100, help='Maximum number of epochs to run for')
+    args = parser.parse_args()
+
+    model_name = args.model
+    unsupervised_batch_size = 1000
+    batch_size = 100
+
+    dataset_name = 'MNIST'
+    datasets = load_MNIST_data(100, 2000, True, True)
+
+    if not os.path.exists(state_path):
+        os.mkdir(state_path)
+    if not os.path.exists('{}/{}'.format(state_path, model_name)):
+        os.mkdir('{}/{}'.format(state_path, model_name))
 
     epochs_list = []
     losses_list = []
     validation_accs_list = []
     results_list = []
     for i in range(5):
-        epochs, losses, validation_accs = model.train(*dataloaders)
+        model, train_dataloaders, test_dataloader = \
+            get_models_and_dataloaders(model_name, dataset_name, datasets, batch_size, unsupervised_batch_size)
+
+        epochs, losses, validation_accs = model.train(args.max_epochs, train_dataloaders)
         results = model.test(test_dataloader)
 
         epochs_list.append(epochs)
