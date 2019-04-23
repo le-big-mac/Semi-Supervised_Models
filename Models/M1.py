c707e68939c1b58e9e6c5826623b0870b2355bcd
Charlie <cl685@cam.ac.uk>
2019-04-23 14:05:42 +0100

    m1 hyperparameter loop

--- Models/M1.py
+++ Models/M1.py
@@ -4,20 +4,21 @@ from torch.nn import functional as F
 from Models.BuildingBlocks import VAE, Classifier
 from Models.Model import Model
 from utils.trainingutils import EarlyStopping, unsupervised_validation_loss
+import pickle
 
 
 class M1(Model):
     def __init__(self, input_size, hidden_dimensions_encoder, latent_size, hidden_dimensions_classifier,
-                 num_classes, output_activation, dataset_name, device):
+                 num_classes, output_activation, lr, dataset_name, device):
         super(M1, self).__init__(dataset_name, device)
 
         self.VAE = VAE(input_size, hidden_dimensions_encoder, latent_size, output_activation).to(device)
-        self.VAE_optim = torch.optim.Adam(self.VAE.parameters(), lr=1e-3)
+        self.VAE_optim = torch.optim.Adam(self.VAE.parameters(), lr=lr)
         self.Encoder = self.VAE.encoder
 
         self.Classifier = Classifier(latent_size, hidden_dimensions_classifier, num_classes).to(device)
         self.Classifier_criterion = nn.CrossEntropyLoss(reduction='sum')
-        self.Classifier_optim = torch.optim.Adam(self.Classifier.parameters(), lr=1e-3)
+        self.Classifier_optim = torch.optim.Adam(self.Classifier.parameters(), lr=lr)
 
         self.model_name = 'm1'
 
@@ -155,3 +156,62 @@ class M1(Model):
         z, _, _ = self.Encoder(data)
 
         return self.Classifier(z)
+
+
+def hyperparameter_loop(dataset_name, dataloaders, input_size, num_classes, max_epochs, device):
+    hidden_layer_vae_size = min(500, (input_size + num_classes) // 2)
+    hidden_layer_classifier_size = 50
+    hidden_layers_vae = range(1, 3)
+    hidden_layers_classifier = range(0, 2)
+    z_size = [200, 100, 50]
+    param_combinations = [(i, j, k) for i in hidden_layers_vae for j in hidden_layers_classifier for k in z_size]
+    lr = 1e-3
+
+    unsupervised, supervised, validation, test = dataloaders
+    train_dataloaders = (unsupervised, supervised, validation)
+    num_labelled = len(supervised.dataset)
+
+    best_acc = 0
+    best_path = None
+    best_params = None
+
+    f = open('./results/{}/m1_{}_labelled_hyperparameter_train.csv'.format(dataset_name, num_labelled), 'ab')
+    for p in param_combinations:
+        print('M1 params {}'.format(p))
+
+        h_v, h_c, z = p
+
+        model = M1(input_size, h_v * [hidden_layer_vae_size], z, h_c * [hidden_layer_classifier_size], num_classes,
+                   lambda x: x, lr, dataset_name, device)
+        epochs, losses, val_accs = model.train_model(max_epochs, train_dataloaders, False)
+        validation_result = model.test_model(validation)
+
+        model_path = './state/m1/{}_{}_{}_{}_{}'.format(dataset_name, num_labelled, h_v, h_c, z)
+        torch.save(model.state_dict(), model_path)
+
+        params = {'input size': input_size, 'hidden layers vae': h_v * [hidden_layer_vae_size],
+                  'hidden layers classifier': h_c * [hidden_layer_classifier_size], 'latent dim': z,
+                  'num classes': num_classes}
+        logging = {'accuracy': validation_result, 'epochs': epochs, 'losses': losses, 'accuracies': validation_result,
+                   'params': params, 'filepath': model_path}
+
+        pickle.dump(logging, f)
+
+        if validation_result > best_acc:
+            best_acc = validation_result
+            best_path = model_path
+            best_params = params
+
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+
+    f.close()
+
+    hidden_v = best_params['hidden layers vae']
+    hidden_c = best_params['hidden layers classifier']
+    latent = best_params['latent dim']
+    model = M1(input_size, hidden_v, latent, hidden_c, num_classes, lambda x: x, lr, dataset_name, device)
+    model.load_state_dict(torch.load(best_path))
+    test_acc = model.test_model(test)
+
+    return test_acc
