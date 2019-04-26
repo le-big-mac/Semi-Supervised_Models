8bac6c7de0dc67d8f486a154fe935222e6ac0d17
Charlie <cl685@cam.ac.uk>
2019-04-26 12:27:29 +0100

    mse loss nan test

--- nan_test.py
+++ nan_test.py
@@ -28,7 +28,8 @@ class VAE(nn.Module):
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        # return torch.sigmoid(self.fc4(h3))
+        return self.fc4(h3)
 
     def forward(self, x):
         mu, logvar = self.encode(x)
@@ -39,7 +40,8 @@ class VAE(nn.Module):
 def loss_function(recon_x, x, mu, logvar):
     print('Logvar:')
     print(logvar)
-    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    # BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    BCE = F.mse_loss(recon_x, x, reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
