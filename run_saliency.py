f11cd5d54ba51921da7b3902b43ea3dae820c207
Charlie <cl685@cam.ac.uk>
2019-05-13 19:50:33 +0100

    saliency mnist

--- run_saliency.py
+++ run_saliency.py
@@ -1,37 +1,34 @@
 import torch
-import argparse
-from utils.datautils import load_data_from_file
+from torch.utils.data import DataLoader, TensorDataset
+from utils.datautils import load_MNIST_data, stratified_k_fold
 from Saliency import VanillaSaliency, GuidedSaliency
 import matplotlib.pyplot as plt
+from Models import SimpleNetwork
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-state_path = './Models/state'
 
 saliency_map = {
     'vanilla': VanillaSaliency,
-    'guided':GuidedSaliency
+    'guided': GuidedSaliency
 }
 
 
 def __main__():
-    parser = argparse.ArgumentParser(description='Data and model to compute saliency for')
-    parser.add_argument('model_type', type=str,
-                        choices=['simple', 'pretraining', 'sdae', 'simple_m1', 'm1', 'm2', 'ladder'],
-                        help="Choose which model to run"
-                        )
-    parser.add_argument('model_name')
-    parser.add_argument('data')
-    parser.add_argument('--saliency_type')
+    (data, labels), (t, l) = load_MNIST_data()
 
-    args = parser.parse_args()
-    model_type = args.model_type
+    model = SimpleNetwork(784, [400, 400], 10, 1e-3, 'saliency', device, 'simple_saliency', './outputs/saliency')
 
-    data = load_data_from_file(args.data)
+    indices = stratified_k_fold(data, labels, 2)
+    train, val = next(indices)
 
-    # TODO: no good way of loading models up
+    train_dataset = TensorDataset(data[train], labels[train])
+    val_dataset = TensorDataset(data[val], labels[val])
+    t_dl = DataLoader(train_dataset, batch_size=100, shuffle=True)
+    v_dl = DataLoader(val_dataset, batch_size=val_dataset.__len__())
 
-    input = test_dataset[input_idx][0].unsqueeze(0)
+    model.train_model(100, (None, t_dl, v_dl), False)
 
+    input = t[12].unsqueeze(0)
     output = model.classify(input)
 
     _, prediction = output.max(1)
@@ -42,44 +39,47 @@ def __main__():
     guided_saliency = GuidedSaliency(model.Classifier, device).generate_saliency(input, prediction)
 
     if device.type == 'cuda':
+        vanilla_saliency = vanilla_saliency.cpu()
         guided_saliency = guided_saliency.cpu()
 
-    pos_map = guided_saliency.clamp(min=0)
-    pos_map = pos_map / pos_map.max()
+    for s in [(vanilla_saliency, 'vanilla'), (guided_saliency, 'guided')]:
+        saliency, string = s
 
-    neg_map = - guided_saliency.clamp(max=0)
-    neg_map = neg_map / neg_map.max()
+        pos_map = saliency.clamp(min=0)
+        pos_map = pos_map / pos_map.max()
 
-    abs_map = guided_saliency.abs()
-    abs_map = abs_map / abs_map.max()
+        neg_map = - saliency.clamp(max=0)
+        neg_map = neg_map / neg_map.max()
 
-    input = input.detach()
-    input = input.view(28, 28)
-    pos_map = pos_map.view(28, 28)
-    neg_map = neg_map.view(28, 28)
-    abs_map = abs_map.view(28, 28)
-    #
-    # plt.imsave('original.png', input, cmap='gray')
-    # plt.imsave('pos.png', pos_map, cmap='gray')
-    # plt.imsave('neg.png', neg_map, cmap='gray')
-    # plt.imsave('abs.png', abs_map, cmap='gray')
+        abs_map = saliency.abs()
+        abs_map = abs_map / abs_map.max()
 
-    figure = plt.figure(figsize=(8, 8), facecolor='w')
+        input = input.detach()
+        input = input.view(28, 28)
+        pos_map = pos_map.view(28, 28)
+        neg_map = neg_map.view(28, 28)
+        abs_map = abs_map.view(28, 28)
 
-    plt.subplot(2, 2, 1)
-    plt.title("Original Image")
-    plt.imshow(input, cmap="gray")
+        figure = plt.figure(figsize=(8, 8), facecolor='w')
 
-    plt.subplot(2, 2, 2)
-    plt.title("Positive Saliency")
-    plt.imshow(pos_map, cmap='gray')
+        plt.subplot(2, 2, 1)
+        plt.title("Original Image")
+        plt.imshow(input, cmap="gray", interpolation=None)
 
-    plt.subplot(2, 2, 3)
-    plt.title("Negative Saliency")
-    plt.imshow(neg_map, cmap='gray')
+        plt.subplot(2, 2, 2)
+        plt.title("Positive Saliency")
+        plt.imshow(pos_map, cmap='gray', interpolation=None)
 
-    plt.subplot(2, 2, 4)
-    plt.title("Absolute Saliency")
-    plt.imshow(abs_map, cmap='gray')
+        plt.subplot(2, 2, 3)
+        plt.title("Negative Saliency")
+        plt.imshow(neg_map, cmap='gray', interpolation=None)
 
-    plt.show()
+        plt.subplot(2, 2, 4)
+        plt.title("Absolute Saliency")
+        plt.imshow(abs_map, cmap='gray', interpolation=None)
+
+        plt.savefig('./outputs/saliency/{}_saliency_maps.pdf'.format(string))
+
+
+if __name__ == "__main__":
+    __main__()
